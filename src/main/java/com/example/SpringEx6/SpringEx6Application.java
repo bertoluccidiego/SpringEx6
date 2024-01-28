3457ef9220b799199a9c58eb9a6911fd98d661d6
Diego C. Bertolucci <bertolucci.diego.c@gmail.com>
2024-01-28 14:06:03 -0300

    Initial commit

---
+++ src/main/java/com/example/SpringEx6/SpringEx6Application.java
@@ -0,0 +1,13 @@
+package com.example.SpringEx6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEx6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEx6Application.class, args);
+	}
+
+}
